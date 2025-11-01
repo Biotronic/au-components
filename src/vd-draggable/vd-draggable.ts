ef2e5f7a5c6760623f1f7a187be2ef573d96513d
Biotronic <simen.kjaras@gmail.com>
2025-11-01 13:35:14 +0100

    Documentation

--- src/vd-draggable/vd-draggable.ts
+++ src/vd-draggable/vd-draggable.ts
@@ -4,24 +4,49 @@ import { autoinject, bindable } from 'aurelia-framework';
 
 @autoinject
 export class VdDraggable {
+  // The type of object being dragged.
+  // Used for filtering in vd-drop-target.
   @bindable
-  public type: string;
+  public type: string = 'unspecified';
+  // The actual object being dragged. Duh.
   @bindable
   public data: any;
-  
+  // Whether this thing can be dragged right now.
+  @bindable
+  public active: boolean = true;
+  @bindable
+  public dropEffectsAllowed: ('copy' | 'link' | 'move')[] = ['copy', 'link', 'move'];
+
   private draggableElement: HTMLElement;
 
   attached() {
-    this.draggableElement.draggable = true;
+    const findWithPrefix = (arr: Readonly<string[]>, prefix: string) => arr.find(e => e.startsWith(prefix)).substring(prefix.length);
+
+    const getEffects = () => {
+      let effects: 'none' | 'copy' | 'copyLink' | 'copyMove' | 'link' | 'linkMove' | 'move' | 'all' | 'uninitialized' =
+        ['copy', 'link', 'move']
+          .filter((a: any) => this.dropEffectsAllowed.includes(a))
+          .map((a, i) => i == 0 ? a : (a[0].toUpperCase() + a.substring(1)))
+          .join('') as any;
+      switch (effects as any) {
+        case '': effects = 'none'; break;
+        case 'copyLinkMove': effects = 'all'; break;
+      }
+      return effects;
+    }
+
     this.draggableElement.addEventListener('dragstart', (e: DragEvent) => {
       const id = dragStorage.send(this.data);
       e.dataTransfer.setData(dragIdMimeType + id, id);
       e.dataTransfer.setData(dragTypeMimeType + this.type, this.type);
+
+      e.dataTransfer.effectAllowed = getEffects();
       this.draggableElement.classList.add('dragging');
     });
+
     this.draggableElement.addEventListener('dragend', (e: DragEvent) => {
       this.draggableElement.classList.remove('dragging');
-      const id = e.dataTransfer.types.find(t => t.startsWith(dragIdMimeType)).substring(dragIdMimeType.length);
+      const id = findWithPrefix(e.dataTransfer.types, dragIdMimeType);
       setTimeout(() => {
         dragStorage.unsend(id);
       }, 10000);
--- src/vd-drop-target/vd-drop-target.ts
+++ src/vd-drop-target/vd-drop-target.ts
@@ -5,55 +5,62 @@ import { autoinject, bindable } from 'aurelia-framework';
 interface IDropEvent {
   data: any;
   type: string;
+  // x, y coordinates relative to dropTargetElement.
   x: number;
   y: number;
+  // The original drag event.
+  event: DragEvent;
 }
 
 @autoinject
 export class VdDropTarget {
+  // Which types of object that can be dropped on this drop target.
+  // This matches the type field on vd-draggable.
   @bindable
-  public accept: string | string[] = [];
+  public accept: string | string[] = ['unspecified'];
+  // Basically just what cursor to show - will also be filtered against dropEffectsAllowed on vd-draggable.
+  // You probably don't need to touch this.
   @bindable
   public dropEffect: 'copy' | 'move' | 'link' = 'move';
+  // Whether the drop target will accept drops right now.
   @bindable
   public active = true;
+  // Called repeatedly while dragging.
+  // Useful for updating previews, e.g.
+  // Return false to indicate that this is not the right spot (not enough space, e.g.)
   @bindable
   public dragFunc: (event: IDropEvent) => boolean;
+  // Called when the dragged object leaves.
+  // Useful for cleaning up previews, e.g.
+  @bindable
+  public dragLeaveFunc: (event: IDropEvent) => void;
+  // Called when the drag operation ends with it being dropped here.
+  // You should probably bind this and handle the result.
   @bindable
   public dropFunc: (event: IDropEvent) => void;
-  @bindable
-  public dropCancelFunc: (event: IDropEvent) => void;
 
   private dropTargetElement: HTMLElement;
 
-  private isTypeAccepted(type: string): boolean {
-    if (type.startsWith('linx/drag-type:')) {
-      type = type.substring('linx/drag-type:'.length);
-    }
-
-    let accept: string[] = [];
-    if (typeof this.accept === 'string') {
-      accept = this.accept.split(',').map(t => t.trim());
-    } else {
-      accept = this.accept;
-    }
-    return accept.includes(type);
-  }
-
   attached() {
+    const findWithPrefix = (arr: Readonly<string[]>, prefix: string) => arr.find(e => e.startsWith(prefix)).substring(prefix.length);
+
     const test = (e: DragEvent) => {
-      return this.active && e.dataTransfer.types.some(t => this.isTypeAccepted(t));
+      let type = findWithPrefix(e.dataTransfer.types, dragTypeMimeType);
+      let accept = typeof (this.accept) == 'string' ? [...this.accept.split(',')] : this.accept;
+      return this.active && accept.includes(type);
     }
+
     const createEvent = (e: DragEvent): IDropEvent => {
-      const type = e.dataTransfer.types.find(t => t.startsWith(dragTypeMimeType));
-      const id = e.dataTransfer.types.find(t => t.startsWith(dragIdMimeType)).substring(dragIdMimeType.length);
+      const type = findWithPrefix(e.dataTransfer.types, dragTypeMimeType);
+      const id = findWithPrefix(e.dataTransfer.types, dragIdMimeType);
       const msg = dragStorage.get(id);
       const rect = this.dropTargetElement.getBoundingClientRect();
       return {
         data: msg,
-        type: type ? type.substring(dragTypeMimeType.length) : '',
+        type: type,
         x: e.clientX - rect.left,
-        y: e.clientY - rect.top
+        y: e.clientY - rect.top,
+        event: e
       };
     }
 
@@ -63,17 +70,20 @@ export class VdDropTarget {
       }
       this.dropTargetElement.classList.add('drag-over');
     });
+
     this.dropTargetElement.addEventListener('dragleave', (e: DragEvent) => {
       this.dropTargetElement.classList.remove('drag-over');
       this.dropTargetElement.classList.remove('drag-refuse');
-      if (this.dropCancelFunc) {
-        this.dropCancelFunc(createEvent(e));
+      if (this.dragLeaveFunc) {
+        this.dragLeaveFunc(createEvent(e));
       }
     });
+
     this.dropTargetElement.addEventListener('dragover', (e: DragEvent) => {
       if (!test(e)) {
         return;
       }
+      e.dataTransfer.dropEffect = this.dropEffect;
       if (this.dragFunc) {
         if (this.dragFunc(createEvent(e)) === false) {
           this.dropTargetElement.classList.add('drag-refuse');
@@ -81,15 +91,16 @@ export class VdDropTarget {
         }
       }
       e.preventDefault();
-      e.dataTransfer.dropEffect = this.dropEffect;
     });
+    
     this.dropTargetElement.addEventListener('drop', (e: DragEvent) => {
       this.dropTargetElement.classList.remove('drag-over');
       this.dropTargetElement.classList.remove('drag-refuse');
+      const id = findWithPrefix(e.dataTransfer.types, dragIdMimeType);
       if (!test(e)) {
+        dragStorage.unsend(id);
         return;
       }
-      const id = e.dataTransfer.types.find(t => t.startsWith(dragIdMimeType)).substring(dragIdMimeType.length);
       e.preventDefault();
       if (this.dropFunc) {
         this.dropFunc(createEvent(e));
